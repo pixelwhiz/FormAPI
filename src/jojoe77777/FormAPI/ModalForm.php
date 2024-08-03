3e9029c4232a49c6818968b53c38100d4c30c4a6
pixelwhiz <daffa01futn@gmail.com>
2024-08-03 17:43:02 +0700

    initial commit

--- src/jojoe77777/FormAPI/ModalForm.php
+++ src/jojoe77777/FormAPI/ModalForm.php
@@ -25,7 +25,7 @@ class ModalForm extends Form {
 
     public function processData(&$data) : void {
         if(!is_bool($data)) {
-            throw new FormValidationException("Expected a boolean response, got " . gettype($data));
+            $data = false;
         }
     }
 
